a7ad1a8caeb55df326c4baada00903ea30652947
Nafeesa Aziz <langdenafeesa@gmail.com>
2017-12-06 18:33:48 +0530

    First commit //Database details

--- InterviewDemoProject1/src/g/DBInitializer.java
+++ InterviewDemoProject1/src/g/DBInitializer.java
@@ -2,6 +2,7 @@ package g;
 
 public interface DBInitializer 
 {
+	//Database details
 	String DRIVER="oracle.jdbc.driver.OracleDriver";
 	String URL="jdbc:oracle:thin:@localhost:1521:xe";
 	String USERNAME="system";
